c4d0a26cff1c4c7d770854a91de5126a157354d5
Мужецкий Антон <tonich2001@gmail.com>
2022-03-07 12:26:40 +0300

    fix: del extra /

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { // http://localhost:3000
-		w.Header().Set("Access-Control-Allow-Origin", "https://xenodochial-mayer-d916ec.netlify.app/") // url to deployed front 
+		w.Header().Set("Access-Control-Allow-Origin", "https://xenodochial-mayer-d916ec.netlify.app") // url to deployed front 
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization, X-CSRF-Token, Location")
