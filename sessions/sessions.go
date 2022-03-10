f00565f2ffab76b271df813b77161674789a34f6
Kislv <vityakiselev96@gmail.com>
2022-03-10 20:47:06 +0300

    try to change path

--- sessions/sessions.go
+++ sessions/sessions.go
@@ -38,7 +38,8 @@ func FinishSession(w http.ResponseWriter, r *http.Request, id uint64) error {
 	}
 	sessionId, isIn := session.Values["id"]
 	if isIn && id == sessionId {
-		session.Options.MaxAge = -1
+		// session.Options.MaxAge = -1
+		session.Options.Path = "/a/a/a/a/a/a/aa/"
 		err := session.Save(r, w)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
