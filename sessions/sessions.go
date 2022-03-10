8b3758f388bbdfd8d3a61a158b1a077559ff1c15
Kislv <vityakiselev96@gmail.com>
2022-03-10 20:22:31 +0300

    set MaxAge = 0

--- sessions/sessions.go
+++ sessions/sessions.go
@@ -38,7 +38,7 @@ func FinishSession(w http.ResponseWriter, r *http.Request, id uint64) error {
 	}
 	sessionId, isIn := session.Values["id"]
 	if isIn && id == sessionId {
-		session.Options.MaxAge = -1
+		session.Options.MaxAge = 0
 		err := session.Save(r, w)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
