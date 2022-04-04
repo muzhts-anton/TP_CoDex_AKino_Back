6c24164d39e9d6d3e4acb33ef2c6c2b3d8fe2711
Konstantin <k.v.kostinich@mail.ru>
2022-04-04 23:52:52 +0300

    изменил ссылку на веб-сервер

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://park-akino.ru") // url to deployed front
+		w.Header().Set("Access-Control-Allow-Origin", "https://park-akino.ru") // url to deployed front
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization, X-CSRF-Token, Location")
