0dc61b2ff862bb093de597df5bc14231d0690798
Kislv <vityakiselev96@gmail.com>
2022-04-20 18:10:25 +0300

    delete csrf

--- internal/pkg/middlewares/middlewares.go
+++ internal/pkg/middlewares/middlewares.go
@@ -9,8 +9,8 @@ func Cors(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", "https://park-akino.ru") // url to deployed front
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization, X-CSRF-Token, Location")
-		w.Header().Set("Access-Control-Expose-Headers", "Authorization, X-CSRF-Token")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization, Location")
+		w.Header().Set("Access-Control-Expose-Headers", "Authorization")
 		w.Header().Set("Access-Control-Max-Age", "600")
 		if r.Method == "OPTIONS" {
 			return
