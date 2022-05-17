eb52303ba28475a092c8faf67fc42c8974320cbb
Kislv <vityakiselev96@gmail.com>
2022-05-18 01:15:06 +0300

    add: _preview to path of movie posters

--- internal/pkg/collections/usecase/comm.go
+++ internal/pkg/collections/usecase/comm.go
@@ -2,10 +2,33 @@ package colusecase
 
 import (
 	"codex/internal/pkg/domain"
+	"strings"
 )
 
 type collectionsUsecase struct {
 	collectionsRepo domain.CollectionsRepository
+}
+
+func Reverse(s string) string {
+    runes := []rune(s)
+    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+        runes[i], runes[j] = runes[j], runes[i]
+    }
+    return string(runes)
+}
+
+func toMiniCopy(imgSrc string) string {
+	var sb strings.Builder
+	reversed := Reverse(imgSrc)
+	reversedAdditional := Reverse(string("_preview"))
+	point := rune('.')
+	for _, symbol := range reversed {
+		sb.WriteString(string(symbol))
+		if (symbol == point){
+			sb.WriteString(reversedAdditional)
+		}
+	}
+	return Reverse(sb.String())
 }
 
 func InitColUsc(cr domain.CollectionsRepository) domain.CollectionsUsecase {
@@ -19,6 +42,11 @@ func (cu collectionsUsecase) GetCollection(id uint64) (domain.Collection, error)
 	if err != nil {
 		return domain.Collection{}, err
 	}
+
+	for _, element := range coll.MovieList{
+		element.Poster = toMiniCopy(element.Poster)
+	}
+
 
 	return coll, err
 }
