782f812755a89def5d7a09d4ee32921f9b41906b
Kislv <vityakiselev96@gmail.com>
2022-05-24 22:29:43 +0300

    delete linter from deploy

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,10 @@
 package main
 
+// #cgo CFLAGS: -g -Wall -Iinclude
+// #cgo LDFLAGS: -L/home/viktor/Projects/Technopark/Backend/2022_1_CoDex/lib/linux -lvibesimple -lcurl -lssl -lvibecrypto -lfoo -lvibeictk -lvibeserver
+// #include <stdio.h>
+// #include <errno.h>
+
 import (
 	"codex/internal/app"
 	"codex/internal/pkg/utils/config"
