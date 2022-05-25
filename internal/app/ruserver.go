7d2342845695a84eea3aa31376de17ed58cd6741
Kislv <vityakiselev96@gmail.com>
2022-05-25 18:13:00 +0300

    add case in notifications

--- internal/app/ruserver.go
+++ internal/app/ruserver.go
@@ -160,7 +160,7 @@ func notificationWorker(announcedRepo domain.AnnouncedRepository) {
 				}
 				comingAnnounced.RUnlock()
 			}
-			time.Sleep(24 * time.Hour)
+			time.Sleep(5 * time.Minute)
 		}
 	}
 	// for {
